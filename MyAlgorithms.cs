b6de6755a576f88146acec2a39fe84027aa1568b
Mstaheri <mstaheri.011@gmail.com>
2023-08-18 17:30:03 +0330

    Add project files.

---
+++ MyAlgorithms.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class MyAlgorithms
+    {
+        public int Factorial(int number) 
+        {
+            int factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial*= i;
+            }
+            return factorial;
+        }
+        public bool LinearSearcher(string[] Nam , string search)
+        {
+            for (int i = 0; i < Nam.Length; i++)
+            {
+                if (Nam[i] == search)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool BinarySearcher(int[] Nam, int search)
+        {
+            int Left = 0;
+            int Right = Nam.Length - 1;
+            while (Left <= Right)
+            {
+                int Mid = (Left + Right) / 2;
+                if (Nam[Mid] == search)
+                {
+                    return true;
+                }
+                else if (Nam[Mid] > search)
+                {
+                    Right = Mid -1;
+                }
+                else if (Nam[Mid] < search)
+                {
+                    Left = Mid + 1;
+                }
+            }
+            return false;
+
+        }
+        ///////////////////////////////////////////////////// //
+        public int[] BinaryInsertionSort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int key = arr[i];
+                int left = 0;
+                int right = i - 1;
+
+                // استفاده از جستجوی دودویی برای یافتن مکان مناسب برای قرار دادن عنصر
+                while (left <= right)
+                {
+                    int mid = (left + right) / 2;
+
+                    if (key < arr[mid])
+                        right = mid - 1;
+                    else
+                        left = mid + 1;
+                }
+
+                // شیفت دادن عناصر به راست برای ایجاد جای خالی برای قرار دادن عنصر
+                for (int j = i - 1; j >= left; j--)
+                {
+                    arr[j + 1] = arr[j];
+                }
+
+                arr[left] = key;
+            }
+            return arr;
+        }
+        public int[] BubbleSorter(int[] Nam)
+        {
+            for (int i = 0; i < Nam.Length - 1; i++)
+            {
+                for (int x = 0; x < Nam.Length - 1; x++)
+                {
+                    if (Nam[x] > Nam[x + 1])
+                    {
+                        int a = Nam[x];
+                        Nam[x] = Nam[x + 1];
+                        Nam[x + 1] = a;
+                    }
+                }
+            }
+            return Nam;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Algorithms;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+while(true) 
+{
+    Console.WriteLine("1.Factorial");
+    Console.WriteLine("2.LinearSearcher");
+    Console.WriteLine("3.BinarySearcher");
+    Console.WriteLine("4.FastSearcher");
+    Console.WriteLine("5.BinaryInsertionSorter");
+    Console.WriteLine("6.BubbleSorter");
+    int a = int.Parse(Console.ReadLine());
+
+    if (a == 1)
+    {
+        Console.Write("Please enter the number :");
+        int number = int.Parse(Console.ReadLine());
+
+        MyAlgorithms algorithms = new MyAlgorithms();
+        int factorial = algorithms.Factorial(number);
+        Console.WriteLine(factorial.ToString());
+    }
+    else if (a == 2)
+    {
+        string[] nam = new string[5];
+        for (int i = 0; i < nam.Length; i++)
+        {
+            Console.Write("Please enter the  Name :");
+            nam[i] = Console.ReadLine();
+        }
+        Console.Write("Please enter the search :");
+        string search = Console.ReadLine();
+
+        MyAlgorithms algorithms = new MyAlgorithms();
+        bool result = algorithms.LinearSearcher(nam,search);
+        Console.WriteLine(result.ToString());
+    }
+    else if (a == 3)
+    {
+        int[] number = new int[5];
+        for (int i = 0; i < number.Length; i++)
+        {
+            Console.Write("Please enter the  number :");
+            number[i] = int.Parse(Console.ReadLine());
+        }
+        Console.Write("Please enter the search :");
+        int search = int.Parse(Console.ReadLine());
+
+        MyAlgorithms algorithms = new MyAlgorithms();
+        bool result = algorithms.BinarySearcher(number, search);
+        Console.WriteLine(result.ToString());
+    }
+    else if (a == 4)
+    {
+        int[] number = new int[10];
+        for (int i = 0; i < number.Length; i++)
+        {
+            Console.Write("Please enter the  Name :");
+            number[i] = int.Parse(Console.ReadLine());
+        }
+        Console.Write("Please enter the search :");
+        int search = int.Parse(Console.ReadLine());
+
+        MyAlgorithms algorithms = new MyAlgorithms();
+        bool result = algorithms.BinarySearcher(number, search);
+        Console.WriteLine(result.ToString());
+    }
+    else if (a == 5)
+    {
+        int[] number = new int[10];
+        for (int i = 0; i < number.Length; i++)
+        {
+            Console.Write("Please enter the  number :");
+            number[i] = int.Parse(Console.ReadLine());
+        }
+
+        MyAlgorithms algorithms = new MyAlgorithms();
+        number = algorithms.BinaryInsertionSort(number);
+        for (int i = 0; i < number.Length; i++)
+        {
+            Console.WriteLine(number[i]);
+        }
+    }
+    else if (a == 6)
+    {
+        int[] number = new int[10];
+        for (int i = 0; i < number.Length; i++)
+        {
+            Console.Write("Please enter the  Name :");
+            number[i] = int.Parse(Console.ReadLine());
+        }
+
+        MyAlgorithms algorithms = new MyAlgorithms();
+        number = algorithms.BubbleSorter(number);
+        for (int i = 0; i < number.Length; i++)
+        {
+            Console.WriteLine(number[i]);
+        }
+    }
+}
